430ff448cdb2ad65414c3504b7326678216f0a27
39506KK <107116943+39506KK@users.noreply.github.com>
2022-07-20 07:26:30 +0000

    [Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images.page(params[:page])
+    @post_images = @user.post_image.page(params[:page])
   end
 
   def edit
