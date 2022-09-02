8f969f502b5b064f7e29e7b4a531c8c103534ddf
EC2 Default User <ec2-user@ip-172-31-10-35.ap-northeast-1.compute.internal>
2022-09-02 14:54:26 +0000

    PF7章学習

--- db/migrate/20220715122231_rename_shop_name_column_to_string.rb
+++
@@ -1,5 +0,0 @@
-class RenameShopNameColumnToString < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :post_images, :stop_name, :shop_name
-  end
-end
--- db/migrate/20220715122728_rename_caption_column_to_string.rb
+++
@@ -1,5 +0,0 @@
-class RenameCaptionColumnToString < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :post_images, :capion, :caption
-  end
-end
